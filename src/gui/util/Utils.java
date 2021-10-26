0c7296e4be8250b2099340dfba4470ad993eec15
LorenzoCVB <cvb.lorenzo@gmail.com>
2021-10-26 15:48:26 -0300

    saving a new department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -1,5 +1,6 @@
 package gui.util;
 
+//import gui.FXML;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.stage.Stage;
@@ -8,5 +9,14 @@ public class Utils {
 
 	public static Stage currentStage(ActionEvent event) {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
+	}
+	
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);	
+		}
+		catch(NumberFormatException e) {
+			return null;
+		}
 	}
 }
